15c53c04bae2b1c110a90fba1d410f41c873347c
Daniel Lister <dan.lister@gmail.com>
2015-12-05 17:15:22 -0500

    show the slope

--- winterwizardjam/game.py
+++ winterwizardjam/game.py
@@ -21,6 +21,9 @@ class game(object):
     def run(self):
         start_time = sdl2hl.timer.get_ticks()
         frames = 0
+
+        tangent_x = 0
+
         while True:
             frames += 1
             for event in sdl2hl.events.poll():
@@ -29,19 +32,38 @@ class game(object):
                     print 1000.0*frames / (end_time - start_time)
                     sdl2hl.quit()
                     sys.exit()
+                elif event.type == sdl2hl.KEYDOWN and event.keycode == sdl2hl.KeyCode.left:
+                    tangent_x -= 1
+                elif event.type == sdl2hl.KEYDOWN and event.keycode == sdl2hl.KeyCode.right:
+                    tangent_x += 1
 
 
 
             self.renderer.draw_color = (0,0,0,255)
             self.renderer.clear()
             self.renderer.draw_color = (255,255,255,255)
+
             rects = []
+            points = []
             for x in range(0, self.window_size[0]):
                 y = self.window_size[1] - int(self.geometry.height(x))
                 #self.renderer.draw_point(x,y)
                 r = sdl2hl.Rect(x, y, 0, int(self.geometry.height(x)))
                 rects.append(r)
+                # points.append(sdl2hl.Point(x,y))
+                # points.append(sdl2hl.Point(x,self.window_size[1]))
                 # self.renderer.draw_rect(r)
             self.renderer.draw_rects(*rects)
+            # self.renderer.draw_lines(*points)
+
+            ##draw the slope
+            self.renderer.draw_color = (255, 0, 0, 255)
+            x = tangent_x
+            y = self.window_size[1] - int(self.geometry.height(x))
+
+            p1 = (int(x) - 100, int(y + 100 * self.geometry.slope(x)))
+            p2 = (int(x + 100), int(y - 100 * self.geometry.slope(x)))
+
+            self.renderer.draw_line(p1[0], p1[1], p2[0], p2[1])
 
             self.renderer.present()
--- winterwizardjam/geometry.py
+++ winterwizardjam/geometry.py
@@ -1,13 +1,16 @@
-from math import sin, pi
+from math import cos, sin, pi
 
 class geometry(object):
 
     def __init__(self):
-        self.slope = 0.5
+        self.line_slope = 0.5
         self.sin_1_amplitude = 100
         self.sin_1_period = 1000
         self.sin_2_amplitude = 150
         self.sin_2_period = 1300
 
-    def height(self,x):
-        return self.slope * x + self.sin_1_amplitude * sin ((2*pi/ self.sin_1_period) * x) + self.sin_2_amplitude * sin ((1 / self.sin_2_period) * x)
+    def height(self, x):
+        return self.line_slope * x + self.sin_1_amplitude * sin ((2*pi / self.sin_1_period) * x) + self.sin_2_amplitude * sin ((2*pi / self.sin_2_period) * x)
+
+    def slope(self, x):
+        return self.line_slope + self.sin_1_amplitude * (2*pi / self.sin_1_period) * cos ((2*pi / self.sin_1_period) * x) + self.sin_2_amplitude * (2*pi / self.sin_2_period) * cos ((2*pi / self.sin_2_period) * x)
