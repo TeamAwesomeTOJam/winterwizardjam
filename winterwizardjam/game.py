025d345594739268b434e1dfe1120e363da1de09
Daniel Lister <dan.lister@gmail.com>
2015-12-05 16:39:19 -0500

    draw with rects and print fps

--- winterwizardjam/game.py
+++ winterwizardjam/game.py
@@ -12,15 +12,21 @@ class game(object):
 
         self.geometry = geometry.geometry()
 
-        self.window_size = (1024, 576)
+        self.window_size = (1920, 1080)
 
         self.window = sdl2hl.Window(title="Winter Wizard Jam",w=self.window_size[0], h=self.window_size[1])
+        #self.renderer = sdl2hl.Renderer(self.window,-1, sdl2hl.RendererFlags.presentvsync)
         self.renderer = sdl2hl.Renderer(self.window)
 
     def run(self):
+        start_time = sdl2hl.timer.get_ticks()
+        frames = 0
         while True:
+            frames += 1
             for event in sdl2hl.events.poll():
                 if event.type == sdl2hl.QUIT:
+                    end_time = sdl2hl.timer.get_ticks()
+                    print 1000.0*frames / (end_time - start_time)
                     sdl2hl.quit()
                     sys.exit()
 
@@ -29,8 +35,13 @@ class game(object):
             self.renderer.draw_color = (0,0,0,255)
             self.renderer.clear()
             self.renderer.draw_color = (255,255,255,255)
+            rects = []
             for x in range(0, self.window_size[0]):
                 y = self.window_size[1] - int(self.geometry.height(x))
-                self.renderer.draw_point(x,y)
+                #self.renderer.draw_point(x,y)
+                r = sdl2hl.Rect(x, y, 0, int(self.geometry.height(x)))
+                rects.append(r)
+                # self.renderer.draw_rect(r)
+            self.renderer.draw_rects(*rects)
 
             self.renderer.present()
