e9ad7cf37a52ff15f4324af5e380268fed7cfd61
Daniel Lister <dan.lister@gmail.com>
2015-12-09 20:49:53 -0500

    stop drawing mouse

--- winterwizardjam/game.py
+++ winterwizardjam/game.py
@@ -101,11 +101,6 @@ class game(object):
             self.renderer.draw_color = (255, 0, 0, 255)
             self.renderer.draw_line(self.camera.to_screen_x(p1x), self.camera.to_screen_y(p1y), self.camera.to_screen_x(p2x), self.camera.to_screen_y(p2y))
 
-            # draw the mouse
-            self.renderer.draw_color = (0, 255, 0, 255)
-            r = sdl2hl.Rect(self.camera.to_screen_x(self.mouse_x) - 5, self.camera.to_screen_y(self.mouse_y) - 5, 10, 10)
-            self.renderer.draw_rect(r)
-
             # draw the kite
             self.kite.draw(self.renderer, self.camera, self.player.x, self.player.y, self.player.kite_angle)
 
